2d1950499411c002480eb11f7a0191b90d3725b3
lv1821 <miguel.espinoza228638@potros.itson.edu.mx>
2023-02-09 12:50:06 -0700

    update

--- app/src/main/java/mx/edu/potros/calculadora/examenu1/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/calculadora/examenu1/MainActivity.kt
@@ -4,30 +4,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var etC: EditText
-    lateinit var etF: EditText
 
-    val btnGC:Button=findViewById(R.id.btnGC)
-    val btnGF:Button=findViewById(R.id.btnGF)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val cc:TextView= findViewById(R.id.etC)
+        val cf:TextView= findViewById(R.id.etF)
+        val btnGC: Button=findViewById(R.id.btnGC)
+        val btnGF: Button=findViewById(R.id.btnGF)
+
         btnGC.setOnClickListener {
-            fun calcularC(etC: Double): Double{
-                return (etC-32)/1.8
+            var gradosF: Double=0.0
+            try{
+                gradosF=cf.text.toString().toDouble
+            } catch (ex: java.lang.Exception){
+
             }
+
+            var gradosC=convertirGC(gradosF)
+            val formatted ="%.2f".format(gradosC)
+            cc.setText(formattedNumber)
         }
 
         btnGF.setOnClickListener {
-            fun calcularF(etF: Double): Double{
-                return etF*1.8+32
+            var gradosC: Double=0.0
+            try{
+                gradosC=cc.text.toString().toDouble
+            } catch (ex: java.lang.Exception){
+
             }
+
+            var gradosF=convertirGC(gradosC)
+            val formatted ="%.2f".format(gradosC)
         }
 
+        convertirGC(gradosF: Double):Double{
+            return (gradosF-32)/1.8
+        }
+        convertirGF(gradosC: Double):Double{
+            return (gradosC*1.8)+32;
+        }
     }
 }
